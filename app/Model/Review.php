0ed605976005d3ad2c2a5a4dfdd5532a247f3968
ahmedmohamed1101140 <ahmedmohamed1101140@outlook.com>
2018-09-01 11:21:47 +0200

    adding review to th product

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     //
+    protected $fillable =[
+        'customer','rating','review'
+    ];
+
     public function product(){
         return $this->belongsTo(Product::class);
     }
